9fb5c9816231f1ab744778c2038c977ea121f9ec
BG1288 <JONAST@danskebank.dk>
2023-09-23 00:16:00 +0200

    Minor improvements to encoding service

--- src/domain/services/encoding_service.py
+++ src/domain/services/encoding_service.py
@@ -7,19 +7,21 @@ from keras.src.utils import pad_sequences
 
 class EncodingService:
 
-    def encode_str(self,
-                   tokenizer: Tokenizer,
-                   length: int,
-                   lines: List[str]) -> np.ndarray[int]:
+    def __init__(self, lines: List[str]) -> None:
+        self.tokenizer = self.compute_tokenizer(lines)
+
+    def encode_list(self,
+                    length: int,
+                    lines: List[str]) -> np.ndarray[int]:
         """
         This method is responsible to encode a string into a vector of integers using Kera's
         and padding the sequences with 0's to the desired length
         """
-        seq = tokenizer.texts_to_sequences(lines)
+        seq = self.tokenizer.texts_to_sequences(lines)
         seq = pad_sequences(seq, maxlen=length, padding='post')
         return seq
 
-    def compute_token(self, lines: np.ndarray) -> Tokenizer:
+    def compute_tokenizer(self, lines: List[str]) -> Tokenizer:
         """
         Compute the tokens using Keras package
         """
