ea7e2192c0a78fde7e4fcd8c4c8d6c33ffaaf2cf
BG1288 <JONAST@danskebank.dk>
2023-09-24 00:01:07 +0200

    add model training, but ran out of memory

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+C
+req/requirement.txt,c\f\cf56fb14fb4419ff170f2ee48637fa92fbdc7b23
--- src/domain/services/encoding_service.py
+++ src/domain/services/encoding_service.py
@@ -7,24 +7,24 @@ from keras.src.utils import pad_sequences
 
 class EncodingService:
 
-    def __init__(self, lines: List[str]) -> None:
-        self.tokenizer = self.compute_tokenizer(lines)
+    def __init__(self, tokens: List[str]) -> None:
+        self.tokenizer = self.compute_tokenizer(tokens)
 
-    def encode_list(self,
-                    length: int,
-                    lines: List[str]) -> np.ndarray[int]:
+    def encode_tokens(self,
+                      length: int,
+                      tokens: List[str]) -> np.ndarray[int]:
         """
-        This method is responsible to encode a string into a vector of integers using Kera's
+        Responsible to encode tokens into a vector of integers using Kera's
         and padding the sequences with 0's to the desired length
         """
-        seq = self.tokenizer.texts_to_sequences(lines)
+        seq = self.tokenizer.texts_to_sequences(tokens)
         seq = pad_sequences(seq, maxlen=length, padding='post')
         return seq
 
-    def compute_tokenizer(self, lines: List[str]) -> Tokenizer:
+    def compute_tokenizer(self, tokens: List[str]) -> Tokenizer:
         """
         Compute the tokens using Keras package
         """
         tokenizer = Tokenizer()
-        tokenizer.fit_on_texts(lines)
+        tokenizer.fit_on_texts(tokens)
         return tokenizer
